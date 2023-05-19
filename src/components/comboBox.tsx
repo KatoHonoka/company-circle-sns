0e11a4ad9574837213df4f1e10139be88f31d257
Yuri_Minemura <yuri.minemura@rakus-partners.co.jp>
2023-05-19 19:00:56 +0900

    サークル（島）作成画面と一部機能、コンポーネントの作成

---
+++ src/components/comboBox.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+
+// Optionsには選択項目、htmlForには<label>のhtmlFor属性の値
+export default function ComboBox({ Options, htmlFor }:any){
+
+    const [inputValue, setInputValue] = useState('');
+    const [options, setOptions] = useState<string[]>([]);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        setInputValue(value);
+    
+        // ユーザーの入力に基づいて選択肢をフィルタリングする
+        const filteredOptions = Options.filter((ops: string | string[]) =>
+          ops.includes(value)
+        );
+        setOptions(filteredOptions);
+      };
+    
+      const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const selectedOption = event.target.value;
+        setInputValue(selectedOption);
+      };
+    
+  
+    return (
+    <div >
+        <input type="text" value={inputValue}
+                onChange={handleInputChange}
+                id={htmlFor}
+                />
+      <select value={inputValue} onChange={handleSelectChange}>
+        <option value="">選択</option>
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
