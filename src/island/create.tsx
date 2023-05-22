23313ad7b40d0758587f84cc497575b103b04fe6
Yuri_Minemura <yuri.minemura@rakus-partners.co.jp>
2023-05-22 09:34:32 +0900

    サークル作成画面の余分項目の削除

--- src/island/create.tsx
+++ src/island/create.tsx
@@ -1,90 +1,87 @@
 import React, { ChangeEvent, useEffect, useState } from "react";
-import styles from '../styles/islandCreate.module.css'
+import styles from "../styles/islandCreate.module.css";
 import ComboBox from "../components/comboBox";
 
-
-
-export default function IslandCreate(){
-
+export default function IslandCreate() {
   const [imageUrl, setImageUrl] = useState("/login/loginCounter.png");
 
   // データベースのデータを取ってくる
-  const userOptions= ['山田', '山本', '佐藤', '鈴木', '田中'];
-  const tagOptions=['サッカー', '大人数', '野外', '野球'];
+  const userOptions = ["山田", "山本", "佐藤", "鈴木", "田中"];
+  const tagOptions = ["サッカー", "大人数", "野外", "野球"];
 
-  // CSS部分で画像URLを変更（imgタグ以外で挿入すれば、円形にしても画像が収縮表示されない）  
+  // CSS部分で画像URLを変更（imgタグ以外で挿入すれば、円形にしても画像が収縮表示されない）
   useEffect(() => {
     let circleElement = document.getElementById("img");
     if (circleElement) {
-          circleElement.style.backgroundImage = `url('${imageUrl}')`;
+      circleElement.style.backgroundImage = `url('${imageUrl}')`;
     }
   }, [imageUrl]);
 
   // 画像ファイル選択したら、表示画像に反映
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      const file = e.target.files?.[0];
-      if (file) {
-          const url = URL.createObjectURL(file);
-          setImageUrl(url);}
-        };
-        
+    const file = e.target.files?.[0];
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setImageUrl(url);
+    }
+  };
+
   // タグ追加
-  const addHandler=()=>{
-  }
+  const addHandler = () => {};
 
-  // 島作成する   
-  const createHandler=()=>{
-  }
- 
+  // 島作成する
+  const createHandler = () => {};
 
-    return(
-        <div className={styles.background}>
-        <div className={styles.box}>
+  return (
+    <div className={styles.background}>
+      <div className={styles.box}>
         <h1>島作成</h1>
         <div className={styles.inputs}>
-            <div>
-                <label htmlFor="islandName">島名<span>【必須】</span></label>
-                <input type="text" id='islandName' className={styles.inputA}/>
-            </div>
+          <div>
+            <label htmlFor="islandName">
+              島名<span>【必須】</span>
+            </label>
+            <input type="text" id="islandName" className={styles.inputA} />
+          </div>
 
-            <div>
-                <label htmlFor="detail">活動内容<span>【必須】</span></label>
-                <input type="text" id='detail' className={styles.inputA}/>
-            </div>
+          <div>
+            <label htmlFor="detail">
+              活動内容<span>【必須】</span>
+            </label>
+            <input type="text" id="detail" className={styles.inputA} />
+          </div>
 
-            <div>
-                <label htmlFor="user">メンバー</label>
-                <div className={styles.displaySide}>
-                <ComboBox Options={userOptions} htmlFor='user'/>   
-                </div>
+          <div>
+            <label htmlFor="user">メンバー</label>
+            <div className={styles.displaySide}>
+              <ComboBox Options={userOptions} htmlFor="user" />
             </div>
+          </div>
 
+          <div>
+            <p className={styles.icon} id="img"></p>
+            <label htmlFor="thumbnail">サムネイル</label>
+            <input
+              type="file"
+              id="thumbnail"
+              className={styles.inputA}
+              onChange={handleFileChange}
+            />
+          </div>
 
-            <div>
-                <p className={styles.icon} id='img' ></p>
-                <label htmlFor="thumbnail">サムネイル</label>
-                <input type="file" id='thumbnail' className={styles.inputA} onChange={handleFileChange}/>
-            </div>
-            
-            <div>
-                <label htmlFor="email">ログインID（登録メールアドレス）</label>
-                <input type="email" id='email' className={styles.inputA}/>
-            </div>
+          <div>
+            <label htmlFor="tag">タグ</label>
+            <ComboBox Options={tagOptions} id="tag" />
+          </div>
 
-            <div>
-                <label htmlFor="tag">タグ</label>
-                <ComboBox Options={tagOptions} id ='tag' />
-            </div>
-
-            <div>
-                <label htmlFor="addTag">タグ追加</label>
-                <input type="text" id='addTag' className={styles.inputA}/>
-                <button onClick={addHandler}>追加</button>
-            </div>
-            <button onClick={createHandler}>新しい島生活を始める</button>
+          <div>
+            <label htmlFor="addTag">タグ追加</label>
+            <input type="text" id="addTag" className={styles.inputA} />
+            <button onClick={addHandler}>追加</button>
+          </div>
+          <button onClick={createHandler}>新しい島生活を始める</button>
         </div>
-
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
