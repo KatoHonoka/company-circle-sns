5038cf4a9fd7c91d9c81f31281e4abd5f025fa80
Yuri_Minemura <yuri.minemura@rakus-partners.co.jp>
2023-05-22 10:02:33 +0900

    イベントのメニューバー項目修正

--- src/components/menubarEvent.tsx
+++ src/components/menubarEvent.tsx
@@ -1,64 +1,56 @@
 import React, { useEffect } from "react";
-import styles from '../styles/menubar.module.css'
+import styles from "../styles/menubar.module.css";
 import { Link } from "react-router-dom";
 
-
 export default function MenubarEvent(thumbnail: any) {
+  useEffect(() => {
+    let imageUrl = "/login/loginCounter.png";
+    let circleElement = document.getElementById("img");
 
-    useEffect(() => {
-        let imageUrl = "/login/loginCounter.png";
-        let circleElement = document.getElementById("img");
-    
-        if (circleElement) {
-          circleElement.style.backgroundImage = `url('${imageUrl}')`;
-        }
-      }, []);
-    
-    // ユーザーがイベントに参加してるとき
-    // if(){
-    return(
-        <div className={styles.menubar}>
-             <div className={styles.icon} id='img'></div>
-             <h4>イベント名</h4>
-             <div className={styles.menuContents}>
-             <div>
-             <Link to={`/island/forum`}>掲示板</Link>
-             </div>
-             <div>
-             <Link to={`/event/[id]`}>イベント</Link>
-             </div>
-             <div>
-             <Link to={`/island/post`}>ポスト</Link>
-             </div>
-             <div>
-             <Link to={`/island/menbers`}>島民一覧</Link>
-             </div>
-             <div>
-             <Link to={`/island/[id]`}>島詳細</Link>
-             </div>
-             </div>
+    if (circleElement) {
+      circleElement.style.backgroundImage = `url('${imageUrl}')`;
+    }
+  }, []);
+
+  // ユーザーがイベントに参加してるとき
+  // if(){
+  return (
+    <div className={styles.menubar}>
+      <div className={styles.icon} id="img"></div>
+      <h4>イベント名</h4>
+      <div className={styles.menuContents}>
+        <div>
+          <Link to={`/event/forum`}>掲示板</Link>
         </div>
+        <div>
+          <Link to={`/event/post`}>ポスト</Link>
+        </div>
+        <div>
+          <Link to={`/event/menbers`}>参加者一覧</Link>
+        </div>
+        <div>
+          <Link to={`/event/[id]`}>イベント詳細</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+// ユーザーがイベントに参加してないとき
+// else if(){
+// return(
+//     <>
+//     <div className={styles.menubar}>
+//     <div className={styles.icon} id='img'></div>
+//      <h4>イベント名</h4>
+//          <div>
+//          <Link to={`/event/menbers`}>参加者一覧</Link>
+//          </div>
+//          <div>
+//          <Link to={`/event/[id]`}>イベント詳細</Link>
+//          </div>
+//     </div>
+//     </>
 
-    )}
-    // ユーザーがイベントに参加してないとき
-    // else if(){
-        // return(
-        //     <>
-        //     <div className={styles.menubar}>
-        //     <div className={styles.icon} id='img'></div>
-        //      <h4>イベント名</h4>
-        //          <div>
-        //          <Link to={`/event/[id]`}>イベント</Link>
-        //          </div>
-        //          <div>
-        //          <Link to={`/island/menbers`}>島民一覧</Link>
-        //          </div>
-        //          <div>
-        //          <Link to={`/island/[id]`}>島詳細</Link>
-        //          </div>
-        //     </div>
-        //     </>
-
-        // )
-    // }
+// )
+// }
 // }
--- src/user/login.tsx
+++ src/user/login.tsx
@@ -1,34 +1,44 @@
-import React from "react"
-import Menubar from "../components/menubarIsland"
-import styles from '../styles/login.module.css'
+import React from "react";
+import Menubar from "../components/menubarIsland";
+import styles from "../styles/login.module.css";
 
+export default function Login() {
+  const loginHandler = () => {};
+  return (
+    <>
+      <div className={styles.box}>
+        <div className={styles.imgCovered}>
+          <img
+            src="/login/loginCounter.png"
+            alt="Login Counter"
+            className={styles.img}
+          />
+        </div>
+        <div className={styles.board}>
+          <h4 className={styles.boardTitle}>ログイン申請書</h4>
+          <div>
+            <input
+              type="email"
+              id="email"
+              placeholder="ログインID（登録メールアドレス）"
+              className={styles.inputA}
+            />
+          </div>
 
-export default function Login(){
+          <div>
+            <input
+              type="password"
+              id="pass"
+              placeholder="パスワード"
+              className={styles.inputB}
+            />
+          </div>
+        </div>
 
-    const loginHandler =()=>{
-
-    }
-    return(
-        <>
-            <div className={styles.box}>
-            <img src='/login/loginCounter.png' alt='Login Counter' className={styles.img}/>
-            <div className={styles.board}>
-                <h4 className={styles.boardTitle}>ログイン申請書</h4>
-                <div>
-                    <input type="email" id='email' placeholder="ログインID（登録メールアドレス）" className={styles.inputA}/>
-                </div>
-
-                <div>
-                    <input type="password" id='pass' placeholder="パスワード" className={styles.inputB}/>
-                </div>
-            </div>
-
-            <button onClick={loginHandler} className={styles.button}>ログイン</button>
-            </div>
-            {/* </div> */}
-
-        
-        </>
-        
-    )
+        <button onClick={loginHandler} className={styles.button}>
+          ログイン
+        </button>
+      </div>
+    </>
+  );
 }
