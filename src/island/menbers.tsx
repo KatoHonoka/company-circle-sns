d9da04a2fb24b895d40ae7b1e0cccccf65b42e34
Hayaka3a <miho.hayakawa@rakus-partners.co.jp>
2023-05-22 14:44:06 +0900

    島民一覧画面作成

--- src/island/menbers.tsx
+++ src/island/menbers.tsx
@@ -1,9 +1,17 @@
-import ParticipantList from "../components/participantList/ParticipantList";
+import ParticipantList from "../components/ParticipantList";
+import MenubarIsland from "../components/menubarIsland";
+import styles from "../styles/participantList.module.css";
 
 export default function IslandMenbers() {
+  //島情報のデータを取得
+  const islandData = { ここでデータを取得: undefined, thumbnail: "imageURL" };
+
   return (
     <>
-      <ParticipantList table={"island"} />
+      <div className={styles.display}>
+        <MenubarIsland thumbnail={islandData.thumbnail} />
+        <ParticipantList table={"island"} />
+      </div>
     </>
   );
 }
--- types/index.tsx
+++ types/index.tsx
@@ -1,8 +1,53 @@
 import { type } from "os";
 
+export type Island = {
+  islandID: number;
+  islandName: string;
+  thumbnail: string;
+  detail: string;
+  ownerID: number;
+  createdAt: number;
+  creratedBy: string;
+  updatedAt: number;
+  updatedBy: string;
+  status: boolean;
+};
+
 export type Person = {
-  id: number;
-  family_name: string;
-  first_name: string;
+  userID: number;
+  familyName: string;
+  firstName: string;
+  familyNameKana: string;
+  firstNameKana: string;
+  mailAddress: string;
+  password: string;
   icon: string;
+  employeeCode: number;
+  department: string;
+  createdAt: number;
+  creratedBy: string;
+  updatedAt: number;
+  updatedBy: string;
+  status: boolean;
+};
+
+export type LoginUser = {
+  userID: number;
+  familyName: string;
+  firstName: string;
+  icon: string;
+};
+
+export type Event = {
+  eventID: string;
+  eventName: string;
+  detail: string;
+  startDate: Date;
+  endDate: Date;
+  thumbnail: string;
+  createdAt: number;
+  creratedBy: string;
+  updatedAt: number;
+  updatedBy: string;
+  status: boolean;
 };
