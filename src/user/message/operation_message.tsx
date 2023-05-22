e4fb1b450d484e2eb1605c96aaaee5d306c24030
KatoHonoka <honoka.kato.pv@rakus-partners.co.jp>
2023-05-22 15:11:42 +0900

    メッセージ全文表示画面の実装

--- src/user/message/operation_message.tsx
+++ src/user/message/operation_message.tsx
@@ -1,25 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "../../styles/user/operation_message_post.module.css";
 
 export default function OperationMessage(){
 
     return(
-        <>
-            <div>
+        <div className={styles.back}>
                 <Link to={`/user/post`}>
-                <img src="/island/close_btn.png" alt="閉じるボタン" />
+                <img src="/island/close_btn.png" 
+                     alt="閉じるボタン" 
+                     className={styles.close_btn}
+                />
                 </Link>
-                <p>from:</p>
-                <img src="/operation_icon.png" alt="お知らせアイコン" />
-                <h3>お知らせ</h3>
-                <p>受信日時:20XX年XX月XX日</p>
-                <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br />
-                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br />
-                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br />
-                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br />
-                テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br />
-                </p>
-            </div>
-        </>
+                <div className={styles.receive}>
+                    <div className={styles.flex}>
+                        <p className={styles.from}>from:</p>
+                        <img src="/operation_icon.png" 
+                             alt="お知らせアイコン" 
+                        />
+                        <h3 className={styles.operation}>お知らせ</h3>
+                        <p className={styles.receiving_time}>受信日時:20XX年XX月XX日</p>
+                    </div>
+                    <p className={styles.text_body}>
+                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br />
+                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br />
+                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br />
+                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br />
+                    テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br />
+                    </p>
+                </div>
+        </div>
     )
 }
