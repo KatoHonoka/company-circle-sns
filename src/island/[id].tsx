b52f506cebb144593a1339f82f73b4787ccffcd9
KatoHonoka <honoka.kato.pv@rakus-partners.co.jp>
2023-05-23 16:43:53 +0900

    サークル詳細画面実装

--- src/island/[id].tsx
+++ src/island/[id].tsx
@@ -1,11 +1,28 @@
 import React from "react";
 import Menubar from "../components/menubarIsland";
 import MenubarIsland from "../components/menubarIsland";
+import styles from "../styles/island/islandDetail.module.css";
 
 export default function IslandDetail() {
   return (
-    <>
-      <MenubarIsland thumbnail="/login/loginCounter.png" />
-    </>
+      <div className={styles.flex}>
+        <MenubarIsland thumbnail="/login/loginCounter.png" />
+        <div className={styles.island_detail}>
+          <img src="/island/island_icon.png" alt="サークルアイコン" />
+            <h2>〇〇島</h2>
+            <p>
+              テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br />
+              テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br />
+              テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br />
+              テキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト<br />
+            </p>
+
+            <div className={styles.btn}>
+              <button>住民申請</button>
+              <button>メッセージを送る</button>
+            </div>
+            <button id={styles.edit_btn}>編集・削除</button>
+        </div>
+      </div>    
   );
 }
