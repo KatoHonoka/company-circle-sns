edfe17481dc81342550ca2c121b75b8dcccc0bfa
Yuri_Minemura <yuri.minemura@rakus-partners.co.jp>
2023-05-23 09:14:04 +0900

    firebase再インストール実行

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,9 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 const firebaseConfig = {
   // Firebase SDK の構成情報をここに追加します
-  // firebaseプロジェクトセットアップを行うとfirebase SDKの構成情報が表示される  
+  // firebaseプロジェクトセットアップを行うとfirebase SDKの構成情報が表示される
 };
 
 firebase.initializeApp(firebaseConfig);
