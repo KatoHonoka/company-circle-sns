4a7756dbc61ccfed97eaf40d2e2007151b1a3647
Yuri_Minemura <yuri.minemura@rakus-partners.co.jp>
2023-05-31 11:26:41 +0900

    退会機能実装

--- src/components/modalWindows/quitConf.tsx
+++ src/components/modalWindows/quitConf.tsx
@@ -1,16 +1,56 @@
 import { useState } from "react";
 import styles from "../../styles/modalWindows/quit.module.css";
-import QuitDone from "./quitDone";
+import GetCookieID from "../cookie/getCookieId";
+import { supabase } from "../../createClient";
 
 export default function QuitConf({
   close2Modal,
   nextOpen2,
+  inputValue,
+  setInputValue,
 }: {
   close2Modal: () => void;
   nextOpen2: () => void;
+  inputValue: string;
+  // React.Dispatch<>は、<>の値を引数として受け取る
+  setInputValue: React.Dispatch<React.SetStateAction<string>>;
 }) {
-  const nextHandler = () => {
-    nextOpen2();
+  const userId = GetCookieID();
+  const [emptyChara, setEmptyChara] = useState("");
+  const [notExist, setNotExist] = useState("");
+
+  const nextHandler = async () => {
+    if (inputValue) {
+      const { data } = await supabase
+        .from("users")
+        .select("familyName, firstName")
+        .eq("id", userId);
+
+      if (data && data.length > 0) {
+        const familyName = data[0].familyName;
+        const firstName = data[0].firstName;
+
+        const fullName = familyName + firstName;
+
+        if (fullName !== inputValue) {
+          setNotExist("入力された名前が間違っています");
+        } else {
+          nextOpen2();
+        }
+      }
+    }
+  };
+
+  const handleInputChange = async (event) => {
+    const value = event.target.value;
+    if (/\s/.test(value)) {
+      // 空白文字（\s）を入れようとした場合、エラーメッセージを表示
+      setEmptyChara("空白文字は入力できません");
+      setNotExist("");
+    } else {
+      setInputValue(value);
+      setEmptyChara("");
+    }
   };
 
   return (
@@ -32,15 +72,32 @@ export default function QuitConf({
                 <p>
                   削除するために下記のテキストボックスに名前をフルネームで入力してください
                 </p>
-                <p>※苗字と名前の間にスペースを入れずに入力してください</p>
+                <p>※姓と名の間にスペースを入れずに入力してください</p>
               </div>
               <div>
-                <input type="text"></input>
+                <input
+                  type="text"
+                  value={inputValue}
+                  onChange={handleInputChange}
+                ></input>
+
+                {emptyChara && (
+                  <div>
+                    <span className={styles.spanB}>{emptyChara}</span>
+                  </div>
+                )}
+                {notExist && (
+                  <div>
+                    <span className={styles.span}>{notExist}</span>
+                  </div>
+                )}
               </div>
             </div>
 
             <div>
-              <button onClick={nextHandler}>アカウントを削除する</button>
+              <button onClick={nextHandler} disabled={!inputValue.trim()}>
+                アカウントを削除する
+              </button>
             </div>
           </div>
         </div>
--- src/user/newUser.tsx
+++ src/user/newUser.tsx
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import LogSt from "../components/cookie/logSt";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 export default function NewUser() {
+  const navigate = useNavigate();
+  const [loginStatus] = useCookies(["loginSt"]);
+
+  // ログイン済みの場合、トップページにリダイレクト
+  useEffect(() => {
+    const status = loginStatus.loginSt;
+    if (status == "true") {
+      navigate("/");
+      window.location.reload();
+    }
+  }, []);
   return <></>;
 }
