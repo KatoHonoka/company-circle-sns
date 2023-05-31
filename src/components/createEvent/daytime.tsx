8976e20277650447b5b39649abb57929c75e6887
Yuri_Minemura <yuri.minemura@rakus-partners.co.jp>
2023-05-31 17:59:06 +0900

    新規イベント作成完了

---
+++ src/components/createEvent/daytime.tsx
@@ -0,0 +1,66 @@
+import { Dispatch, SetStateAction, useState } from "react";
+import styles from "../../styles/island/createIsland.module.css";
+
+export default function Daytime({
+  startDate,
+  setStartDate,
+  endDate,
+  setEndDate,
+}: {
+  startDate: string;
+  setStartDate: Dispatch<SetStateAction<string>>;
+  endDate: string;
+  setEndDate: Dispatch<SetStateAction<string>>;
+}) {
+  const [error, setError] = useState("");
+
+  const handleStartChange = (e) => {
+    setStartDate(e.target.value);
+    // 一文字でも入力されたらエラー削除
+    if (error) {
+      setError("");
+    }
+  };
+
+  const handleEndChange = (e) => {
+    setEndDate(e.target.value);
+    // 一文字でも入力されたらエラー削除
+    if (error) {
+      setError("");
+    }
+  };
+
+  const handleBlur = () => {
+    if (startDate.trim() === "" || endDate.trim() === "") {
+      setError("※開催時期は開始日時と終了日時の両方入力必須項目です");
+    } else {
+      setError("");
+    }
+  };
+  return (
+    <>
+      <input
+        type="date"
+        className={`${styles.inputA} ${error ? styles.errorInput : ""}`}
+        maxLength={300}
+        value={startDate}
+        onChange={handleStartChange}
+        onBlur={handleBlur}
+      />
+      ～
+      <input
+        type="date"
+        className={`${styles.inputA} ${error ? styles.errorInput : ""}`}
+        maxLength={300}
+        value={endDate}
+        onChange={handleEndChange}
+        onBlur={handleBlur}
+      />
+      {error && (
+        <div>
+          <span className={styles.span}>{error}</span>
+        </div>
+      )}
+    </>
+  );
+}
