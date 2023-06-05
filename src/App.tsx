be21bc42509894cd494b3869a85b5a0e05076762
Hayaka3a <miho.hayakawa@rakus-partners.co.jp>
2023-06-05 16:57:10 +0900

    CSS更新

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function App() {
   const showNewUser = window.location.pathname !== "/user/newUser";
 
   return (
-    <div>
+    <div className="App">
       <BrowserRouter>
         {showHeader && showNewUser && <Header />}
         <Routes>
