df22c28c689a2180acf0f0274589263eb45a3342
Yuri_Minemura <yuri.minemura@rakus-partners.co.jp>
2023-06-06 10:28:26 +0900

    難民参加イベントポスト追加

--- src/components/post/belongEvent.tsx
+++ src/components/post/belongEvent.tsx
@@ -14,18 +14,25 @@ export default function BelongEvent() {
           .select("islandID")
           .eq("userID", userID);
 
+        // entrys配列の中からeventIDがnullの値のものを配列から取り除く
+        const ens = entrys.filter((event) => event.islandID !== null);
+
         if (entrysError) {
           console.error("データ1取得失敗", entrysError.message);
           return;
         }
-
         try {
           const filteredEvents = await Promise.all(
-            entrys.map(async (entry) => {
-              const { data: events } = await supabase
+            ens.map(async (en) => {
+              const { data: events, error: eventsError } = await supabase
                 .from("userEntryStatus")
                 .select("eventID")
-                .eq("islandID", entry.islandID);
+                .eq("islandID", en.islandID);
+
+              if (eventsError) {
+                console.error("データ1取得失敗", eventsError.message);
+                return;
+              }
 
               // events配列の中からeventIDがnullの値のものを配列から取り除く
               const filteredEvents = events.filter(
@@ -47,6 +54,22 @@ export default function BelongEvent() {
             return acc;
           }, []);
 
+          const { data: refugee, error: refugeeError } = await supabase
+            .from("userEntryStatus")
+            .select("eventID")
+            .eq("userID", userID);
+
+          if (refugeeError) {
+            console.error("データ取得失敗", refugeeError.message);
+            return;
+          }
+
+          const uniqueEventsFiltered = refugee.filter(
+            (evt) => evt.eventID !== null,
+          );
+
+          uniqueEvents.push(...uniqueEventsFiltered);
+
           await Promise.all(
             uniqueEvents.map(async (event) => {
               const { data: posts } = await supabase
@@ -57,11 +80,17 @@ export default function BelongEvent() {
               try {
                 await Promise.all(
                   posts.map(async (post) => {
-                    const { data: messages } = await supabase
-                      .from("messages")
-                      .select("*")
-                      .eq("postID", post.id)
-                      .eq("isRead", "false");
+                    const { data: messages, error: messagesError } =
+                      await supabase
+                        .from("messages")
+                        .select("*")
+                        .eq("postID", post.id)
+                        .eq("isRead", "false");
+
+                    if (messagesError) {
+                      console.error("データ1取得失敗", messagesError.message);
+                      return;
+                    }
 
                     if (messages[0]) {
                       setHasNewMessage(true);
