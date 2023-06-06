28812c6a0c9e8a6f58c03086a46dd4ec88bf9cce
Yuri_Minemura <yuri.minemura@rakus-partners.co.jp>
2023-06-06 16:25:42 +0900

    サークル作成画面icon追加

--- src/island/create.tsx
+++ src/island/create.tsx
@@ -117,6 +117,7 @@ export default function IslandCreate() {
       islandName: islandName,
       detail: detail,
       ownerID: ownerID,
+      thumbnail: imageUrl,
       status: "false",
     };
 
