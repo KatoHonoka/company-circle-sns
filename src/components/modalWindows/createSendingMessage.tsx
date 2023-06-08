a5348174f9aa3ce35ce38e50992722cd0a169b50
KatoHonoka <honoka.kato.pv@rakus-partners.co.jp>
2023-06-08 17:17:22 +0900

    メッセージ送信日時をデータに格納

--- src/components/modalWindows/createSendingMessage.tsx
+++ src/components/modalWindows/createSendingMessage.tsx
@@ -4,43 +4,38 @@ import { supabase } from "../../createClient";
 import { useParams } from "react-router-dom";
 import GetCookieID from "../cookie/getCookieId";
 
-
 export default function CreateSendingMessage({
-    closeModal,
-    table,
+  closeModal,
+  table,
 }: {
-    closeModal: () => void;
-    table: string;
+  closeModal: () => void;
+  table: string;
 }) {
   const [message, setMessage] = useState("");
   const [postedID, setPostedID] = useState(0);
   const [posts, setPosts] = useState(0);
-  const [ islandName, setIslandName ] = useState("");
+  const [islandName, setIslandName] = useState("");
   const params = useParams();
   const paramsID = parseInt(params.id);
   const userID = GetCookieID();
-
 
   useEffect(() => {
     fetchPost();
     fetchIslandName();
   }, []);
 
-
   const fetchPost = async () => {
-    // userIDから該当のPostIDを割り出す
-    const { data: posts, error: postError } = await supabase
-    .from("posts")
-    .select("id")
-    .eq("userID", userID)
-    .eq("status", false);
+    const { data: postsData, error: postError } = await supabase
+      .from("posts")
+      .select("id")
+      .eq("userID", userID)
+      .eq("status", false);
     if (postError) {
       console.log(userID);
       console.log(postError, "ポストエラー");
     }
-    setPosts(posts[0].id);
+    setPosts(postsData[0]?.id || 0);
 
-    // PostedByに入れるため、送信する側のPostIDを取得する
     const { data: postedBy, error: postedByError } = await supabase
       .from("posts")
       .select("id")
@@ -49,41 +44,42 @@ export default function CreateSendingMessage({
     if (postedByError) {
       console.log(postedByError, "ポストバイエラー");
     }
-    setPostedID(postedBy[0].id);
+    setPostedID(postedBy[0]?.id || 0);
   };
 
   const fetchIslandName = async () => {
-      const { data: island, error: islandError } = await supabase
-        .from("islands")
-        .select("islandName")
-        .eq("id", paramsID)
-        .eq("status", false);
-  
-      if (islandError) {
-        console.log(islandError, "アイランドエラー");
-      } else if (island && island.length > 0) {
-        setIslandName(island[0].islandName);
-      }
-  };
-  
+    const { data: island, error: islandError } = await supabase
+      .from("islands")
+      .select("islandName")
+      .eq("id", paramsID)
+      .eq("status", false);
 
-  // messagesテーブルにメッセージを保存
+    if (islandError) {
+      console.log(islandError, "アイランドエラー");
+    } else if (island && island.length > 0) {
+      setIslandName(island[0].islandName);
+    }
+  };
+
   const sendMessage = async () => {
-    const { data, error } = await supabase
-      .from("messages")
-      .insert([
-        {
-          postID: posts,
-          message: message,
-          scout: false,
-          isRead: false,
-          isAnswered: false,
-          postedBy: postedID,
-          status: false,
-        },
-      ]);
+    const currentDate = new Date();
+    const formattedDate = currentDate.toISOString();
+
+    const { data, error } = await supabase.from("messages").insert([
+      {
+        postID: posts,
+        message: message,
+        scout: false,
+        isRead: false,
+        isAnswered: false,
+        postedBy: postedID,
+        status: false,
+        sendingDate: formattedDate,
+      },
+    ]);
+
     if (error) {
-      console.error("メッセージの送信中にエラーが発生しました:");
+      console.error("メッセージの送信中にエラーが発生しました:", error);
     } else {
       console.log("データが正常に送信されました");
       closeModal();
@@ -96,38 +92,38 @@ export default function CreateSendingMessage({
 
   return (
     <>
-    {islandName && 
-      <div className={styles.overlay}>
-        <div className={styles.modal}>
-          <div className={styles.allContents}>
-            <img
-              src="/modalWindow/close.png"
-              alt="×ボタン"
-              onClick={closeModal}
-              className={styles.close}
-            />
-            <div className={styles.main}>
-              <div className={styles.title}>
-                <h3 className={styles.h3}>{islandName}</h3>
-                <p className={styles.messageName}>メッセージ</p>
+      {islandName && (
+        <div className={styles.overlay}>
+          <div className={styles.modal}>
+            <div className={styles.allContents}>
+              <img
+                src="/modalWindow/close.png"
+                alt="×ボタン"
+                onClick={closeModal}
+                className={styles.close}
+              />
+              <div className={styles.main}>
+                <div className={styles.title}>
+                  <h3 className={styles.h3}>{islandName}</h3>
+                  <p className={styles.messageName}>メッセージ</p>
+                </div>
+                <div className={styles.input}>
+                  <label htmlFor="threadName">コメント</label>
+                  <br />
+                  <textarea
+                    name="text"
+                    className={styles.textSending}
+                    onChange={(e) => setMessage(e.target.value)}
+                  ></textarea>
+                </div>
               </div>
-              <div className={styles.input}>
-                <label htmlFor="threadName">コメント</label><br />
-                <textarea 
-                  name="text" 
-                  className={styles.textSending}
-                  onChange={(e) => setMessage(e.target.value)}
-                >
-                </textarea>
+              <div className={styles.btn}>
+                <button onClick={addHandler}>送信する</button>
               </div>
-            </div>
-            <div className={styles.btn}>
-              <button onClick={addHandler}>送信する</button>
             </div>
           </div>
         </div>
-      </div>
-      }
+      )}
     </>
   );
 }
