79d9819bc64a011ecdd9e4e946187b6e94d76857
Yuri_Minemura <yuri.minemura@rakus-partners.co.jp>
2023-08-08 16:20:26 +0900

    マージ完了

--- src/tests/island/post.test.js
+++ src/tests/island/post.test.js
@@ -35,59 +35,59 @@ describe("IslandPost", () => {
   //       message: "テストメッセージ2",
   //     },
   //   ];
-  //   test("メッセージがない場合の表示確認", async () => {
-  //     // fetchMsgのモック関数を設定(messages[]が空になるように)
-  //     const mockFetchMsg = jest.fn();
-  //     mockFetchMsg.mockResolvedValue([]);
-  //     require("../../components/post/fetchMsg").fetchMsg = mockFetchMsg;
-  //     // readHandlerのモック関数を設定
-  //     const mockReadHandler = jest.fn();
-  //     require("../../components/post/readHandler").readHandler = mockReadHandler;
-  //     render(
-  //       <BrowserRouter>
-  //         <IslandPost />
-  //       </BrowserRouter>,
-  //     );
-  //     expect(screen.getByText("受信メッセージ✉️")).toBeInTheDocument();
-  //     const noMessageText = await screen.findByText("受信メッセージはありません");
-  //     expect(noMessageText).toBeInTheDocument();
-  //     // 「表示」ボタンが表示されていないことを確認 (メッセージがないので表示ボタンもない)
-  //     const displayButton = screen.queryByText("表示");
-  //     expect(displayButton).not.toBeInTheDocument();
-  //     // 「住民許可待ち申請」ボタンが表示されていることを確認
-  //     const entryPermitButton = screen.getByText("住民許可待ち申請");
-  //     expect(entryPermitButton).toBeInTheDocument();
-  //     // 「島に招待する」ボタンが表示されていることを確認
-  //     const inviteButton = screen.getByText("島に招待する");
-  //     expect(inviteButton).toBeInTheDocument();
-  //   });
-  //   test("fetchMsgがメッセージを返してきたとき、メッセージが正しく表示される", async () => {
-  //     // fetchMsgのモック関数を設定(中身のあるmessages[]を返す)
-  //     const mockFetchMsg = jest.fn();
-  //     mockFetchMsg.mockResolvedValue(messages);
-  //     require("../../components/post/fetchMsg").fetchMsg = mockFetchMsg;
-  //     // readHandlerのモック関数を設定
-  //     const mockReadHandler = jest.fn();
-  //     require("../../components/post/readHandler").readHandler = mockReadHandler;
-  //     render(
-  //       <BrowserRouter>
-  //         <IslandPost />
-  //       </BrowserRouter>,
-  //     );
-  //     //messages[]が表示されているか確認
-  //     await waitFor(() => {
-  //       const username1 = screen.findByText(/テストユーザー/);
-  //       expect(username1).toBeInTheDocument();
-  //       const unreadLabel = screen.getByText("未読");
-  //       expect(unreadLabel).toBeInTheDocument();
-  //       const message1 = screen.getByText("テストメッセージ1...");
-  //       expect(message1).toBeInTheDocument();
-  //       const username2 = screen.findByText(/サンプルユーザー/);
-  //       expect(username2).toBeInTheDocument();
-  //       const readLabel = screen.getByText("既読");
-  //       expect(readLabel).toBeInTheDocument();
-  //       const message2 = screen.getByText("テストメッセージ2...");
-  //       expect(message2).toBeInTheDocument();
-  //     });
-  // });
+  test("メッセージがない場合の表示確認", async () => {
+    //   // fetchMsgのモック関数を設定(messages[]が空になるように)
+    //   const mockFetchMsg = jest.fn();
+    //   mockFetchMsg.mockResolvedValue([]);
+    //   require("../../components/post/fetchMsg").fetchMsg = mockFetchMsg;
+    //   // readHandlerのモック関数を設定
+    //   const mockReadHandler = jest.fn();
+    //   require("../../components/post/readHandler").readHandler = mockReadHandler;
+    //   render(
+    //     <BrowserRouter>
+    //       <IslandPost />
+    //     </BrowserRouter>,
+    //   );
+    //   expect(screen.getByText("受信メッセージ✉️")).toBeInTheDocument();
+    //   const noMessageText = await screen.findByText("受信メッセージはありません");
+    //   expect(noMessageText).toBeInTheDocument();
+    //   // 「表示」ボタンが表示されていないことを確認 (メッセージがないので表示ボタンもない)
+    //   const displayButton = screen.queryByText("表示");
+    //   expect(displayButton).not.toBeInTheDocument();
+    //   // 「住民許可待ち申請」ボタンが表示されていることを確認
+    //   const entryPermitButton = screen.getByText("住民許可待ち申請");
+    //   expect(entryPermitButton).toBeInTheDocument();
+    //   // 「島に招待する」ボタンが表示されていることを確認
+    //   const inviteButton = screen.getByText("島に招待する");
+    //   expect(inviteButton).toBeInTheDocument();
+    // });
+    // test("fetchMsgがメッセージを返してきたとき、メッセージが正しく表示される", async () => {
+    //   // fetchMsgのモック関数を設定(中身のあるmessages[]を返す)
+    //   const mockFetchMsg = jest.fn();
+    //   mockFetchMsg.mockResolvedValue(messages);
+    //   require("../../components/post/fetchMsg").fetchMsg = mockFetchMsg;
+    //   // readHandlerのモック関数を設定
+    //   const mockReadHandler = jest.fn();
+    //   require("../../components/post/readHandler").readHandler = mockReadHandler;
+    //   render(
+    //     <BrowserRouter>
+    //       <IslandPost />
+    //     </BrowserRouter>,
+    //   );
+    //   //messages[]が表示されているか確認
+    //   await waitFor(() => {
+    //     const username1 = screen.findByText(/テストユーザー/);
+    //     expect(username1).toBeInTheDocument();
+    //     const unreadLabel = screen.getByText("未読");
+    //     expect(unreadLabel).toBeInTheDocument();
+    //     const message1 = screen.getByText("テストメッセージ1...");
+    //     expect(message1).toBeInTheDocument();
+    //     const username2 = screen.findByText(/サンプルユーザー/);
+    //     expect(username2).toBeInTheDocument();
+    //     const readLabel = screen.getByText("既読");
+    //     expect(readLabel).toBeInTheDocument();
+    //     const message2 = screen.getByText("テストメッセージ2...");
+    //     expect(message2).toBeInTheDocument();
+    //   });
+  });
 });
